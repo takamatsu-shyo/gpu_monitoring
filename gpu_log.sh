1684648a9cb38102455fcd1c1f9939e2cd124694
Sho Yano <takamatsu.shyo@gmail.com>
2019-06-24 03:24:33 +0300

    Add Poser usage and cap

--- gpu_log.sh
+++ gpu_log.sh
@@ -2,5 +2,5 @@
 LOGFILE=$HOME"/my_tools/my_gpu.log"
 
 # datetime, Fan, Temp, GPU-Util
-{ date -uIseconds; nvidia-smi | grep -o "[0-9]*%\|[0-9].C" | tr "\n" ","  ; } | tr "\n" "," >> $LOGFILE
+{ date -uIseconds; nvidia-smi | grep -o "[0-9]*%\|[0-9].C\|[0-9]..W" | tr "\n" ","  ; } | tr "\n" "," >> $LOGFILE
 echo "" >> $LOGFILE
